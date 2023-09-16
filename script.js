3e017357919b28c820f4e2e230b1df9a0343d170
ohneat <144978401+ohneat@users.noreply.github.com>
2023-09-16 00:37:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,58 @@
 // Existing openPhoto function
 function openPhoto(imageSrc) {
-  // ... existing code
+  // Create an overlay element
+  const overlay = document.createElement("div");
+  overlay.style.position = "fixed";
+  overlay.style.top = "0";
+  overlay.style.left = "0";
+  overlay.style.width = "100%";
+  overlay.style.height = "100%";
+  overlay.style.backgroundColor = "rgba(0,0,0,0.9)";
+  overlay.style.display = "flex";
+  overlay.style.justifyContent = "center";
+  overlay.style.alignItems = "center";
+  overlay.style.zIndex = "1000";
+  
+  // Create the image element
+  const img = document.createElement("img");
+  img.src = imageSrc;
+  img.style.maxWidth = "100%";
+  img.style.maxHeight = "100%";
+  img.style.objectFit = "contain";
+  
+  // Add image to overlay
+  overlay.appendChild(img);
+  
+  // Add back button
+  const backButton = document.createElement("button");
+  backButton.textContent = "Back";
+  backButton.style.position = "absolute";
+  backButton.style.top = "16px";
+  backButton.style.left = "16px";
+  backButton.style.padding = "8px 16px";
+  backButton.style.background = "#333";
+  backButton.style.color = "white";
+  backButton.style.fontSize = "16px";
+  backButton.style.cursor = "pointer";
+  
+  backButton.onclick = function() {
+    document.body.removeChild(overlay);
+  };
+
+  // Add back button to overlay
+  overlay.appendChild(backButton);
+
+  // Add overlay to body
+  document.body.appendChild(overlay);
+
+  // Close overlay when clicking outside the image
+  overlay.onclick = function(e) {
+    if (e.target === overlay) {
+      document.body.removeChild(overlay);
+    }
+  };
 }
+
 
 // New code for dynamically adding photos
 const photos = ['roll4/longhorn.jpg', 'roll4/worship_center.jpg', 'roll4/sonic.JPG', 'roll3/fave_tree.jpg', 'roll3/eyes.jpg', 'roll3/light_shadow.jpg', 'roll2/mother_child.jpg', 'roll2/battle_hall.JPG', 'roll2/bananas.jpg', 'roll1/ocho.JPG', 'roll1/path.JPG', 'roll1/water_tower.jpg'];
