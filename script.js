41b7f6249852f23b407b79e81075e111fb28a069
ohneat <144978401+ohneat@users.noreply.github.com>
2023-09-15 23:50:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,53 @@
 function openPhoto(imageSrc) {
-  // Create a new div to hold the full-size image and "Back" button
-  const overlay = document.createElement('div');
-  overlay.style.position = 'fixed';
-  overlay.style.top = '0';
-  overlay.style.left = '0';
-  overlay.style.width = '100%';
-  overlay.style.height = '100vh';
-  overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
-  overlay.style.display = 'flex';
-  overlay.style.justifyContent = 'center';
-  overlay.style.alignItems = 'center';
-  overlay.style.zIndex = '1000';
+  // Create an overlay element
+  const overlay = document.createElement("div");
+  overlay.style.position = "fixed";
+  overlay.style.top = "0";
+  overlay.style.left = "0";
+  overlay.style.width = "100%";
+  overlay.style.height = "100%";
+  overlay.style.backgroundColor = "rgba(0,0,0,0.9)";
+  overlay.style.display = "flex";
+  overlay.style.justifyContent = "center";
+  overlay.style.alignItems = "center";
+  overlay.style.zIndex = "1000";
   
-  // Create an img element for the full-size image
-  const img = document.createElement('img');
+  // Create the image element
+  const img = document.createElement("img");
   img.src = imageSrc;
-  img.style.maxWidth = '100%';
-  img.style.maxHeight = '100%';
-  img.style.objectFit = 'contain';
+  img.style.maxWidth = "100%";
+  img.style.maxHeight = "100%";
+  img.style.objectFit = "contain";
   
-  // Create a "Back" button
-  const backButton = document.createElement('button');
-  backButton.innerText = 'Back';
-  backButton.style.position = 'absolute';
-  backButton.style.top = '16px';
-  backButton.style.left = '16px';
-  backButton.style.padding = '8px 16px';
-  backButton.style.background = '#333';
-  backButton.style.color = 'white';
-  backButton.style.fontSize = '16px';
-  backButton.style.cursor = 'pointer';
+  // Add image to overlay
+  overlay.appendChild(img);
+  
+  // Add back button
+  const backButton = document.createElement("button");
+  backButton.textContent = "Back";
+  backButton.style.position = "absolute";
+  backButton.style.top = "16px";
+  backButton.style.left = "16px";
+  backButton.style.padding = "8px 16px";
+  backButton.style.background = "#333";
+  backButton.style.color = "white";
+  backButton.style.fontSize = "16px";
+  backButton.style.cursor = "pointer";
+  
   backButton.onclick = function() {
     document.body.removeChild(overlay);
   };
 
-  // Append img and "Back" button to the overlay div
-  overlay.appendChild(img);
+  // Add back button to overlay
   overlay.appendChild(backButton);
-  
-  // Append the overlay div to the body
+
+  // Add overlay to body
   document.body.appendChild(overlay);
+
+  // Close overlay when clicking outside the image
+  overlay.onclick = function(e) {
+    if (e.target === overlay) {
+      document.body.removeChild(overlay);
+    }
+  };
 }
